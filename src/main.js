dd0bf31fcdf1eef7386c228005d9385c40b28002
Yuri Sklyarov <yuri.mcalex@gmail.com>
2025-03-01 19:19:17 +0200

    Create main parts of the game

--- src/main.js
+++ src/main.js
@@ -1,3 +1,32 @@
 import './style.css';
 
-console.log('ok');
+// The state of the game
+let state = {};
+
+// The main canvas element and its drawing context
+const canvas = document.getElementById('game');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+const ctx = canvas.getContext('2d');
+
+
+function newGame() {
+  // Reset the game state
+  state = {};
+  draw();
+}
+
+
+function draw() {
+  
+}
+
+
+// Event handlers
+function throwBomb() {
+
+}
+
+function animate(timestamp) {
+  
+}
